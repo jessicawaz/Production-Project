beccee24f35c6fcc3d37ee2a97e4f4b180c1a7ab
jessi <jwazbinski4662@eagle.fgcu.edu>
2020-09-18 09:40:54 -0400

    created a database

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,8 +1,11 @@
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
+
+import java.sql.*;
 
 public class Controller {
 
@@ -17,11 +20,65 @@ public class Controller {
 
     @FXML
     void buttonAddProduct(ActionEvent event) {
-        System.out.println("button pressed");
+        connectToDb();
     }
 
     @FXML
+    private Label lblAdded;
+
+    @FXML
     void buttonRecordProd(ActionEvent event) {
-        System.out.println("button record");
+
+    }
+
+    @FXML
+    private ComboBox<String> cmbItemType;
+
+    // add to comboBox
+    public void initialize() {
+        for (int count = 1; count <= 10; count++) {
+            cmbItemType.getItems().add(String.valueOf(count));
+        }
+//        setEditable(true);
+//        getSelectionModel().selectFirst();
+    }
+
+    public void connectToDb() {
+
+        final String JDBC_DRIVER = "com.h2.Driver";
+        final String DB_URL = "jdbc:h2:./res/db";
+
+        //  Database credentials
+        final String USER = "";
+        final String PASS = "";
+        Connection conn = null;
+        Statement stmt = null;
+
+        try {
+            // STEP 1: Register JDBC driver
+            Class.forName(JDBC_DRIVER);
+
+            //STEP 2: Open a connection
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+            //STEP 3: Execute a query
+            stmt = conn.createStatement();
+
+            String sql = "INSERT INTO product (type, manufacturer, name) " +
+                    "VALUES ( 'AUDIO', 'Apple', 'iPod' )";
+
+            stmt.executeUpdate(sql);
+
+
+            // STEP 4: Clean-up environment
+            stmt.close();
+            conn.close();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
+
